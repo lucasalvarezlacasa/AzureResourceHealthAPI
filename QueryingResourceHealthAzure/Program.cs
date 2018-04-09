d25d146f45a0fb072949eddb4c4cc624d3ad6956
Lucas Alvarez <lucas.alvarez@lumen21.com>
2018-04-09 09:23:44 -0300

    Initial Commit

---
+++ QueryingResourceHealthAzure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QueryingResourceHealthAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var response = SubscriptionHelper.GetSubscriptionResources().GetAwaiter().GetResult();
+            Console.WriteLine(response);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QueryingResourceHealthAzure/SubscriptionHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace QueryingResourceHealthAzure
+{
+    public static class SubscriptionHelper
+    {
+        // Complete this information with your details ------------------------
+        const string tenantId = "";
+        const string appId = "";
+        const string appKey = "";
+        const string subscriptionId = "";
+        // --------------------------------------------------------------------
+
+        const string aadInstance = "https://login.microsoftonline.com/{0}";
+
+        private static string resourceHealthFullUrl = string.Format("https://management.azure.com/subscriptions/{0}/providers/Microsoft.ResourceHealth/availabilityStatuses?api-version=2015-01-01", subscriptionId);
+        private static string registerResourceHealthUrl = string.Format("https://management.azure.com/subscriptions/{0}/providers/Microsoft.ResourceHealth/register?api-version=2015-01-01", subscriptionId);
+
+        public static async Task<string> GetSubscriptionResources()
+        {
+            var authenticationToken = await AuthenticationHelper.AcquireTokenBySPN(tenantId, appId, appKey);
+
+            using (var httpClient = new HttpClient())
+            {
+                // Add the access token to the authorization header of the request.
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authenticationToken);
+
+                // Retrieve information
+                Console.WriteLine("Retrieving resources for subscription...");
+
+                HttpResponseMessage response = await httpClient.GetAsync(resourceHealthFullUrl);
+
+                if (response.IsSuccessStatusCode == true)
+                {
+                    Console.WriteLine("Resources successfully retrieved!");
+
+                }
+                else
+                {
+                    Console.WriteLine("Failed to retrieve list of resources:  {0}\n", response.ReasonPhrase);
+                }
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
